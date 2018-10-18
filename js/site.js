10974afab474931024aa81263e852f92bc094919
codex881 <pruetsapon@hotmail.com>
2018-10-18 22:31:49 +0700

    add accounting web page.

--- js/site.js
+++ js/site.js
@@ -1,36 +1,52 @@
-const config = {
-    apiKey: "AIzaSyAKongB8c-8OWgZPe8Fyb-TreWtMMuDrTo",
-    authDomain: "cross-accounting.firebaseapp.com",
-    databaseURL: "https://cross-accounting.firebaseio.com",
-    projectId: "cross-accounting",
-    storageBucket: "cross-accounting.appspot.com",
-    messagingSenderId: "665881439885"
-};
-firebase.initializeApp(config);
+// const config = {
+//     apiKey: "AIzaSyAKongB8c-8OWgZPe8Fyb-TreWtMMuDrTo",
+//     authDomain: "cross-accounting.firebaseapp.com",
+//     databaseURL: "https://cross-accounting.firebaseio.com",
+//     projectId: "cross-accounting",
+//     storageBucket: "cross-accounting.appspot.com",
+//     messagingSenderId: "665881439885"
+// };
 
 let email = "testlogin881@gmail.com";
 let password = "xxxxxx";
 let name = "Jane";
-let lastname = "Xazdio";
+let lastname = "xyz";
 let displayName = name + " " + lastname;
 let phoneNumber = "0825651452";
 let uid = sessionStorage.uid;
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        console.log(user);
-        console.log("User is signed in.");
-    } else {
-        console.log("No user is signed in.");
-    }
+$(function() {
+    InitializeApp();
 });
+
+function InitializeApp() {
+    $.getJSON("config.json", function(config) {
+        firebase.initializeApp(config);
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                sessionStorage.displayName = user.displayName;
+                sessionStorage.email = user.email;
+                sessionStorage.photoURL = user.photoURL;
+                sessionStorage.uid = user.uid;
+                mainPage();
+            } else {
+                signInPage();
+            }
+        });
+    });
+}
+
+function mainPage() {
+    $('#container').load('includes/main.html');
+}
+
+function signInPage() {
+    $('#container').load('includes/signin.html');
+}
+
+function registerPage() {
+    $('#container').load('includes/register.html');
+}
 
 var userN = "Johnx";
 //delete
@@ -52,69 +68,88 @@ var userN = "Johnx";
 //   console.log(data.val());
 // });
 
-function SignIn()
-{
-    firebase.auth().signInWithEmailAndPassword(email, password).then(function(data) {
-        console.log('Sign in successful.');
+function signIn() {
+    let email = $('input[name=email]').val();
+    let password = $('input[name=password]').val();
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+        sessionStorage.displayName = user.displayName;
+        sessionStorage.email = user.email;
+        sessionStorage.photoURL = user.photoURL;
+        sessionStorage.uid = user.uid;
+        mainPage();
     }).catch(function(error) {
         console.log(error.message);
     });
 }
 
-function SignOut()
-{
+function signOut() {
     firebase.auth().signOut().then(function() {
-        console.log("Sign-out successful.");
+        sessionStorage.clear();
+        signInPage();
     }).catch(function(error) {
         console.log(error.message);
     });
 }
 
-function Register()
-{
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(data) {
-        firebase.database().ref("users/" + data.user.uid + "/").set ({
-            email: email,
-            age: 30
+function register() {
+    let name = $('input[name=name]').val();
+    let lastname = $('input[name=lastname]').val();
+    let email = $('input[name=email]').val();
+    let password = $('input[name=password]').val();
+    let confirmPassword = $('input[name=confirmPassword]').val();
+    let verifRegister = verificationRegister(name, lastname, email, password, confirmPassword);
+    if(verifRegister) {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function(data) {
+            let json = {
+                email: email,
+                name: name,
+                lastname: lastname
+            };
+            firebase.database().ref("users/" + data.user.uid + "/").set (json);
+        }).catch(function(error) {
+            console.log(error.message);
         });
-        console.log('Create User successful.');
-    }).catch(function(error) {
-        console.log(error.message);
-    });
+    }
 }
 
-function UpdateUser()
-{
-    var user = firebase.auth().currentUser;
+function verificationRegister(name, lastname, email, password, confirmPassword) {
+    if(password != confirmPassword)
+    {
+        return false;
+    }
+    return true;
+}
+
+function updateUser() {
+    let user = firebase.auth().currentUser;
     user.updateProfile({
         displayName: displayName,
         photoURL: "https://firebase.google.com/_static/7e8fbbc4f5/images/firebase/lockup.png"
     }).then(function() {
-        firebase.database().ref("users/" + user.uid + "/").update ({
+        let json = {
+            email: email,
             name: name,
             lastname: lastname,
             phoneNumber: phoneNumber
-        });
-        console.log("Update successful.");
+        };
+        firebase.database().ref("users/" + user.uid + "/").update (json);
     }).catch(function(error) {
         console.log(error.message);
     });
 }
 
-function VerificationEmail()
-{
-    var user = firebase.auth().currentUser;
+function verificationEmail() {
+    let user = firebase.auth().currentUser;
     user.sendEmailVerification().then(function() {
-        console.log("Email sent.");
+        // Email sent.
     }).catch(function(error) {
         console.log(error.message);
     });
 }
 
-function ResetPassword()
-{
+function resetPassword() {
     firebase.auth().sendPasswordResetEmail(email).then(function() {
-        console.log("Email sent.");
+        // Email sent.
     }).catch(function(error) {
         console.log(error.message);
     });
